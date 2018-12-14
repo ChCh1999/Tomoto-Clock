6d4c3a4938cc3a153dc437e4833bfcdc2e76b123
subtle12138 <1507802331qq.com>
2018-12-14 10:28:35 +0800

    Create History.cs
    12.14

---
+++ TomatoClock/TomatoClock/History.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class WorkpPlan
+{
+    public int days;
+    public string name;
+    public DateTime time;
+}
+class History
+{
+
+    WorkpPlan[] plans = new WorkpPlan[31];
+
+    public int getDays(WorkpPlan w)
+    {
+        return w.days;
+    }
+
+    public void setDays(WorkpPlan w,int days)
+    {
+        w.days = days;
+    }
+
+    public string getName(WorkpPlan w)
+    {
+        return w.name;
+    }
+
+    public void setName(WorkpPlan w,string name)
+    {
+        w.name = name;
+    }
+
+    public DateTime getTime(WorkpPlan w)
+    {
+        return w.time;
+    }
+
+    public void setTime(WorkpPlan w,DateTime time)
+    {
+        w.time = time;
+    }
+}
+
