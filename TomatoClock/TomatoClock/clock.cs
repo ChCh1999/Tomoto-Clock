7d859ad1857fd6fb01b5b672baaed80c9b296768
my0sotis <1274034280@qq.com>
2018-12-18 00:27:23 +0800

    Clock修改

--- TomatoClock/TomatoClock/clock.cs
+++ TomatoClock/TomatoClock/clock.cs
@@ -6,52 +6,39 @@ namespace TomatoClock
     public class Clock
     {
         //private string workPlan;
-        public DateTime StartTime { set; get; }
-        public TimeSpan PlanTime { set; get; }
-        private TimeSpan MIN_TIMESPAN = new TimeSpan(0, 0, 0, 1);       // +1s
+        private TimeSpan MIN_TIMESPAN = new TimeSpan(0, 0, 0, 1);       // 1s
         private Timer timer = new Timer(1000);                          // 创建一个间隔为1s的Timer
         public TimeSpan RemainedTime;                                   // 剩余时间
-        private bool result = false;                                    // 判断是否完成
+        public DateTime StartTime { set; get; }
+        public TimeSpan PlanTime { set; get; }
+        public bool isFinished = false;                                // 判断是否完成
 
-        public Clock(DateTime start, TimeSpan plan,Clock clock) // 初始化
+        public Clock(DateTime start, TimeSpan plan)                     // 初始化
         {
             StartTime = start;
             PlanTime = plan;
-            RemainedTime = plan;                              
+            RemainedTime = plan;
             timer.Elapsed += TimeEvent;
+        }
+
+        public void Start()
+        {
             timer.Start();
         }
 
-        private void TimeEvent(object sender, ElapsedEventArgs e)            // -1s  到计时
+        private void TimeEvent(object sender, ElapsedEventArgs e)       // -1s  到计时
         {
             while (RemainedTime.TotalSeconds == 0)
             {
+                isFinished = true;
                 timer.Stop();
-                result = true;
             }
             RemainedTime = RemainedTime.Subtract(MIN_TIMESPAN);
         }
 
-        public TimeSpan Stop()              // 返回剩余时间
+        public void Stop()
         {
-            //强制停止
-            if (DateTime.Now - StartTime < PlanTime)
-            {
-                result = false;
-            }
-            //到时间结束
-            else if (DateTime.Now - StartTime == PlanTime)
-            {
-                result = true;
-            }
-            return ReturnTime();
+            timer.Stop();
         }
-
-        public TimeSpan ReturnTime()      // 剩余时间
-        {
-            return PlanTime - (DateTime.Now - StartTime);
-        }
-
-
     }
 }
